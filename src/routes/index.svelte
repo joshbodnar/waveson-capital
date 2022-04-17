566b6b04d2525917525ace4ae6476f1709c37af0
Josh Bodnar <contact.jbodnar@gmail.com>
2022-04-17 00:06:51 -0400

    formatting updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,52 +5,60 @@
 <h1>Bank Search</h1>
 <div class="row">
     <div class="col">
+        <label for="institution" class="form-label">Institution Name</label>
         <input class="form-control"
                placeholder="Institution"
-               type="text" name="search" bind:value={institution} on:keypress={search}>
+               type="text" name="institution" id="institution"
+               bind:value={institution} on:keypress={search}>
     </div>
     <div class="col">
+        <label for="state" class="form-label">State</label>
         <input class="form-control"
                placeholder="State"
-               type="text" name="search" bind:value={usState} on:keypress={search}>
+               type="text" name="state" id="state"
+               bind:value={usState} on:keypress={search}>
     </div>
 </div>
+<hr>
 
 {#if showSearching}
     <p>Searching for {institution}...</p>
 {/if}
 
 {#if showResults}
-<!--    The data is not empty {returnData.data.data}-->
-<h3>count: {returnData.data.totals.count}</h3>
-<table class="table table-striped">
-    <thead>
-    <tr>
-        <th>Cert</th>
-        <th>Name</th>
-        <th>City</th>
-        <th>State</th>
-        <th>Zip</th>
-    </tr>
-    </thead>
-    <tbody>
-    {#each returnData.data.data as d}
-    <tr>
-        <td><a href="/cert/{d.data.CERT}" class="btn btn-primary">{d.data.CERT}</a></td>
-        <td>{d.data.NAME}</td>
-        <td>{d.data.CITY}</td>
-        <td>{d.data.STNAME}</td>
-        <td>{d.data.ZIP}</td>
-    </tr>
-    {/each}
-    </tbody>
-</table>
+    <h3>
+        <small>
+            count: {returnData.data.totals.count}
+        </small>
+    </h3>
+    <table class="table table-sm table-striped">
+        <thead>
+        <tr>
+            <th>Cert</th>
+            <th>Name</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each returnData.data.data as d}
+            <tr>
+                <td><a href="/cert/{d.data.CERT}" class="btn btn-sm btn-primary d-block">{d.data.CERT}</a></td>
+                <td>{d.data.NAME}</td>
+                <td>{d.data.CITY}</td>
+                <td>{d.data.STNAME}</td>
+                <td>{d.data.ZIP}</td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
 {/if}
 
 <script>
     import axios from 'axios'
 
-    let showSearching,showResults = false
+    let showSearching, showResults = false
     let institution, usState = "";
     let returnData = {};
 
